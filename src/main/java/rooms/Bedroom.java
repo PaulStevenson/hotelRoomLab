c4adcbf43544dacf23babc493bfc1726195aa673
Paul <rpmstevenson@gmail.com>
2018-05-18 17:25:41 +0100

    bedroom tests pass

--- src/main/java/rooms/Bedroom.java
+++ src/main/java/rooms/Bedroom.java
@@ -1,4 +1,34 @@
 package rooms;
 
-public class Bedroom {
+import enums.BedroomType;
+
+public class Bedroom extends Room {
+
+    private int number;
+    private BedroomType type;
+    private double roomRate;
+
+    public Bedroom(int number, BedroomType type, double roomRate){
+        super(type.getCapacity());
+        this.number = number;
+        this.type = type;
+        this.roomRate = roomRate;
+    }
+
+    public int getCapacity(){
+        return this.type.getCapacity();
+    }
+
+
+    public int getRoomNumber() {
+        return this.number;
+    }
+
+    public BedroomType getBedroomRoomType() {
+        return this.type;
+    }
+
+    public double getRoomRate() {
+        return this.roomRate;
+    }
 }
---
+++ src/test/java/BedRoomTest.java
@@ -0,0 +1,38 @@
+import enums.BedroomType;
+import org.junit.Before;
+import org.junit.Test;
+import rooms.Bedroom;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class BedRoomTest {
+    
+
+
+    Bedroom bedroom;
+
+    @Before
+    public void before(){
+        bedroom = new Bedroom(666, BedroomType.SINGLE, 50.00);
+    }
+
+    @Test
+    public void hasRoomNumber(){
+        assertEquals(666, bedroom.getRoomNumber());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals(BedroomType.SINGLE, bedroom.getBedroomRoomType());
+    }
+
+    @Test
+    public void getCapacity(){
+        assertEquals(1, bedroom.getCapacity());
+    }
+
+    @Test
+    public void getRoomRate(){
+        assertEquals(50.00, bedroom.getRoomRate(), 0.01);
+    }
+}
